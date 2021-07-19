62b10df0e396b6b3a3bf64b65f247445e7bbcc3e
sebapinery <sebapinery@gmail.com>
2021-07-19 14:32:00 -0300

    get, getById, post, put and deleted methods created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,20 @@
 class Api::V1::UsersController < ApplicationController
-  def get_users
+  before_action :getUser, only: [:update_user, :delete_user, :show_user_by_id]
+
+  def show_user_by_id
+    if @user
+      render json: @user, status: :ok
+    else
+      render json: { json: 'User not found' }, status: :not_found
+    end
+  end
+
+  def show_users
     user = User.all
     if user
-      render json: user, status: :ok 
+      render json: user, status: :ok
+    else
+      render json: { msg: 'Theres no users' }, status: :unprocessable_entity
     end
   end
 
@@ -12,12 +24,41 @@ class Api::V1::UsersController < ApplicationController
     if user.save
       render json: user, status: :ok
     else
-      render json: { messsage: 'User not added' }, status: :unprocessable_entity
+      render json: { msg: 'User not added' }, status: :unprocessable_entity
     end
   end
 
-  private 
+  def update_user
+    if @user
+      if @user.update(userparams)
+        render json: @user, status: :ok
+      else
+        render json: { msg: 'Update failed' }, status: :unprocessable_entity
+      end
+    else
+      render json: { json: 'User not found' }, status: :not_found
+    end
+  end
+
+  def delete_user
+    if @user
+      if @user.destroy
+        render json: { msg: 'User deleted' }, status: :ok
+      else
+        render json: { msg: 'Update failed' }, status: :unprocessable_entity
+      end
+    else
+      render json: { json: 'User not found' }, status: :not_found
+    end
+  end
+
+  private
+
   def userparams
     params.permit(:username, :email, :password_digest)
-  end 
+  end
+
+  def getUser
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  namespace :api do 
+  namespace :api do
     namespace :v1 do
-      post 'add_user', action: :add_user, controller: :users
-      get 'get_users', action: :get_users, controller: :users
+      # get 'get_users', action: :show_users, controller: :users
+      # get 'show_user_by_id', action: :show_user_by_id, controller: :users
+      # post 'add_user', action: :add_user, controller: :users
+      # put 'update_user', action: :update_user, controller: :users
+      # delete 'delete_user', action: :delete_user, controller: :users
+
+      get '/', action: :show_users, controller: :users
+      get '/show_user', action: :show_user_by_id, controller: :users
+      post '/', action: :add_user, controller: :users
+      put '/', action: :update_user, controller: :users
+      delete '/', action: :delete_user, controller: :users
     end
   end
 end
